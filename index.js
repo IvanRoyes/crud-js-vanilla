ba55f9c16bca4629d1f9e2a927a30382aeb5d728
Iván Royes <royesivan@gmail.com>
2026-02-23 19:21:30 -0300

    Mensajes de error con JavaScript

---
+++ index.js
@@ -0,0 +1,62 @@
+function onChangeEmail(){
+        ToggleButtonDisable();
+        toggleEmailErrors();
+}
+
+function onChangePassword() {
+    ToggleButtonDisable();
+    togglePasswordErrors();
+}
+
+function isEmailValid(){
+    const email = document.getElementById("email").value;
+    if (!email) return false;
+    return validateEmail(email);
+}
+
+function isPasswordValid(){
+    const password = document.getElementById('password').value;
+    if (!password) return false;
+    return true;
+}
+
+function validateEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+function toggleEmailErrors(){
+    const email = document.getElementById('email').value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = "block";
+
+    } else {
+        document.getElementById('email-required-error').style.display = "none";
+
+    }
+
+    if (validateEmail(email)){
+        document.getElementById('email-invalid-error').style.display = "none";
+    } else{
+        document.getElementById('email-invalid-error').style.display = "block";
+
+    }
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById('password-required-error').style.display = "block";
+    } else {
+        document.getElementById('password-required-error').style.display = "none";
+    }
+
+}
+
+function ToggleButtonDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+
+}
